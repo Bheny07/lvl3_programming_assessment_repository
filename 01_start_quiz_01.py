194bfdd7165db708876162e71065e557401f6fc4
Bheny07 <hentschelb@middleton.school.nz>
2025-05-05 19:49:06 +1200

    01_start_quiz_01
    Updated comments

--- 01_start_quiz_01.py
+++ 01_start_quiz_01.py
@@ -1,19 +1,18 @@
 """01_start_quiz_01 will load and render a visual representation of the
 New Zealand map"""
 
+# Import tkinter and PIL
 import tkinter as tk
 from tkinter import messagebox
 from PIL import Image, ImageTk
 
 
+# Class to display NZ map
 class AotearoaQuiz(object):
     def __init__(self, roots):
-        # Store the main window object
         self.root = roots
-        # Set the window title
         self.root.title("Aotearoa Names Quiz")
         try:
-            # Open the image file
             self.nz_image = Image.open("Design 2.jpg")
             self.nz_photo = ImageTk.PhotoImage(self.nz_image)
 
@@ -23,19 +22,15 @@ class AotearoaQuiz(object):
             self.root.destroy()
             return
 
-        # Create a canvas for the map
         self.map_canvas = tk.Canvas(roots, width=self.nz_image.width,
                                     height=self.nz_image.height)
-        # Display the image on the canvas
         self.map_canvas.create_image(0, 0, image=self.nz_photo,
                                      anchor=tk.NW)
-        # Make the canvas visible
         self.map_canvas.pack()
 
 
+# Main Loop
 if __name__ == '__main__':
-    # Create the main Tkinter window
     root = tk.Tk()
     quiz = AotearoaQuiz(root)
     root.mainloop()
-
