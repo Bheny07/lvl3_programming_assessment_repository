aa9f555c6688b67ed4152c438904e370d01f7df4
Bheny07 <hentschelb@middleton.school.nz>
2025-05-05 18:08:15 +1200

    01_start_quiz_01
    Adding Comments to code

--- 01_start_quiz_01.py
+++ 01_start_quiz_01.py
@@ -8,9 +8,12 @@ from PIL import Image, ImageTk
 
 class AotearoaQuiz(object):
     def __init__(self, roots):
+        # Store the main window object
         self.root = roots
-        self.root.title("Aotearoa Place Names Quiz")
+        # Set the window title
+        self.root.title("Aotearoa Names Quiz")
         try:
+            # Open the image file
             self.nz_image = Image.open("Design 2.jpg")
             self.nz_photo = ImageTk.PhotoImage(self.nz_image)
 
@@ -20,14 +23,18 @@ class AotearoaQuiz(object):
             self.root.destroy()
             return
 
+        # Create a canvas for the map
         self.map_canvas = tk.Canvas(roots, width=self.nz_image.width,
                                     height=self.nz_image.height)
+        # Display the image on the canvas
         self.map_canvas.create_image(0, 0, image=self.nz_photo,
                                      anchor=tk.NW)
+        # Make the canvas visible
         self.map_canvas.pack()
 
 
 if __name__ == '__main__':
+    # Create the main Tkinter window
     root = tk.Tk()
     quiz = AotearoaQuiz(root)
     root.mainloop()
