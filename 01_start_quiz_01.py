1051372651d7fbefb3acd426219e8d54a4064ffa
Bheny07 <hentschelb@middleton.school.nz>
2025-05-05 17:39:01 +1200

    01_start_quiz_01
    Will load and render a visual representation of the
    New Zealand map (Design 2.jpg)

---
+++ 01_start_quiz_01.py
@@ -0,0 +1,34 @@
+"""01_start_quiz_01 will load and render a visual representation of the
+New Zealand map"""
+
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
+
+class AotearoaQuiz(object):
+    def __init__(self, roots):
+        self.root = roots
+        self.root.title("Aotearoa Place Names Quiz")
+        try:
+            self.nz_image = Image.open("Design 2.jpg")
+            self.nz_photo = ImageTk.PhotoImage(self.nz_image)
+
+        except FileNotFoundError:
+            messagebox.showerror("Error",
+                                 "New Zealand map image not found!")
+            self.root.destroy()
+            return
+
+        self.map_canvas = tk.Canvas(roots, width=self.nz_image.width,
+                                    height=self.nz_image.height)
+        self.map_canvas.create_image(0, 0, image=self.nz_photo,
+                                     anchor=tk.NW)
+        self.map_canvas.pack()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    quiz = AotearoaQuiz(root)
+    root.mainloop()
+
